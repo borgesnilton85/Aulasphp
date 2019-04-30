e0b5d42376cc8c334fe04b367cc2188a20a25e8c
Nilton <borgesnilton85@gmail.com>
2019-04-30 16:35:27 -0300

    Create exemplo03.php

---
+++ Variavel/exemplo03.php
@@ -0,0 +1,24 @@
+<?php
+
+$nome = "Hcode";
+$site = 'www.hcode.com.br';
+$ano = 1990;
+$salario = 5555.55;
+$bloqueio = false;
+////////////////////////////////
+$frutas = array("abacaxi","laranja","manga" );
+//echo $frutas[2];
+
+$nascimento = new DateTime();
+//var_dump($nascimento);
+
+$arquivo = fopen("exemplo03.php","r");
+
+var_dump($arquivo);
+
+$nulo = NULL; // auzencia de valor;
+$vazio =""; 
+
+
+
+ ?>
